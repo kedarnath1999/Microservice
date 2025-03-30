c8dd11c073fbdadab15fbb04fe2b697a37eb87cd
Kedarnath R Gubbi <kedarnathrgubbi@Kedarnaths-MacBook-Air.local>
2025-03-30 18:25:50 -0400

    Initial commit

---
+++ product-service/src/test/java/com/techie/microservices/product/ProductServiceApplicationTests.java
@@ -0,0 +1,59 @@
+package com.techie.microservices.product;
+
+import com.techie.microservices.product.dto.ProductRequest;
+import com.techie.microservices.product.mode.Product;
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.testcontainers.containers.MongoDBContainer;
+
+import java.math.BigDecimal;
+
+import static io.restassured.RestAssured.port;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProductServiceApplicationTests {
+
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.7");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
+	@Test
+	void shouldCreateProduct() {
+		ProductRequest productRequest = getProductRequest();
+		RestAssured.given()
+				.contentType("application/json")
+				.body(productRequest)
+				.when()
+				.post("/api/product")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.body("id", Matchers.notNullValue())
+				.body("name", Matchers.equalTo(productRequest.name()))
+				.body("description", Matchers.equalTo(productRequest.description()))
+				.body("price", Matchers.is(productRequest.price().intValueExact()));
+	}
+	private ProductRequest getProductRequest() {
+		return new ProductRequest("iPhone 13", "iPhone 13", BigDecimal.valueOf(1200));
+	}
+
+}
